8b2d05a1bc5e3941be21c6d0eaa786742eda29af
Renato Rosa Franco <Renato_ny@live.com>
2019-05-07 14:48:42 -0300

    Routes set up

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  
+  # API End Points
+  namespace :api, constraint: { subdomain: 'api' } do
+  	namespace :v1 do
+	  	resources :ideas do
+	  		resources :comments
+	  		resources :reviews
+		  end
+		end
+	end
 end
