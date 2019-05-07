b2fedd2e41286a5441f315392ebcef3e99e4d9af
Renato Rosa Franco <Renato_ny@live.com>
2019-05-07 14:57:07 -0300

    Idea model added

---
+++ app/models/idea.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+class Idea < ApplicationRecord
+	self.table_name = 'ideas'
+	self.primary_kdy = 'id'
+
+	validates :name,
+						presence: true,
+						uniqueness: false,
+						allow_blank: false,
+						length: { minimum: 3, maximum: 50 }
+  
+  validates :description,
+  					presence: true,
+  					uniqueness: false,
+  					allow_blank: false,
+  					length: { minimum: 3, maximum: 30 }
+end
